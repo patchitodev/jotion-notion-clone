9adbaa848415e5a90d0a4f3f70aa9807687a902b
Julio Fuentes <patchitodev@gmail.com>
2023-12-14 17:45:52 -0600

    First commit -- Landing, Auth, Sidebar, User Settings, API endpoints, Sidebar Items, added

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+
+const DocumentsPage = () => {
+  const { user } = useUser();
+
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note",
+    });
+  };
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        width="300"
+        height="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        width="300"
+        height="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button onClick={onCreate}>
+        <PlusCircle className="h-4 w-4 mr-2" /> Create a note
+      </Button>
+    </div>
+  );
+};
+
+export default DocumentsPage;
