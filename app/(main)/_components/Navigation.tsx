6ec1d9ad13946de7ab72e85ea2f9d9549277caa9
Julio Fuentes <patchitodev@gmail.com>
2024-01-11 12:37:57 -0600

    Fixed bug in the navbar. It wasn't showing the option to add a new page in the document list

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -3,6 +3,7 @@
 import {
   ChevronsLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
@@ -144,6 +145,7 @@ const Navigation = () => {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
         </div>
         <div
           onMouseDown={handleMouseDown}
